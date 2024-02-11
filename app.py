8e36c3e89ef5cd36f825d3b1c83b5a2811ecc359
Dmitry_Tyshkevich <tyshkevich.dima@mail.ru>
2024-02-11 19:53:07 +0300

    using routers and creating command buttons

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
 
-bot = Bot(token='6501302268:AAFi4rOOXbHm1tXlu0b3y7VsjdCoJcZTkzQ')
+from handlers.user_private import user_private_router
+from common.bot_cmds_list import private
+
+
+ALLOWED_UPDATES = ["message", "edited_message"]
+
+bot = Bot(token=os.getenv("TOKEN"))
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Это была команда старт')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
-
-
+dp.include_router(user_private_router)
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+
 
 asyncio.run(main())
