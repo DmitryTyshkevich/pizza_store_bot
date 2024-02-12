531176d021401037684606a2af98eb51bc4e18a6
Dmitry_Tyshkevich <tyshkevich.dima@mail.ru>
2024-02-12 20:31:18 +0300

    adding a keyboard and formatting message text

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import asyncio
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+from aiogram.enums import ParseMode
+
 
 from dotenv import find_dotenv, load_dotenv
 
@@ -14,7 +15,7 @@ from common.bot_cmds_list import private
 
 ALLOWED_UPDATES = ["message", "edited_message"]
 
-bot = Bot(token=os.getenv("TOKEN"))
+bot = Bot(token=os.getenv("TOKEN"), parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
@@ -23,6 +24,7 @@ dp.include_router(user_group_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(
         commands=private, scope=types.BotCommandScopeAllPrivateChats()
     )
--- common/bot_cmds_list.py
+++ common/bot_cmds_list.py
@@ -4,6 +4,6 @@ from aiogram.types import BotCommand
 private = [
     BotCommand(command='menu', description='Посмотреть меню'),
     BotCommand(command='about', description='О нас'),
-    BotCommand(command='pyment', description='Варианты оплаты'),
+    BotCommand(command='payment', description='Варианты оплаты'),
     BotCommand(command='shipping', description='Варианты доставки'),
 ]
